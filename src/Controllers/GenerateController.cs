f99e8e5ced91fb1dc582c8f3cb3c48ddd0c91e5f
lars <larstomaslian@gmail.com>
2023-10-06 13:16:28 +0200

    Use ControllerBase.Problem()

--- src/Controllers/GenerateController.cs
+++ src/Controllers/GenerateController.cs
@@ -1,9 +1,9 @@
-using System.Net.Mime;
-using Microsoft.AspNetCore.Mvc;
-using AIWA.API.Models;
-using AIWA.API.Integrations.GPT4;
 using System.Net;
-using Microsoft.AspNetCore.Http.HttpResults;
+using System.Net.Mime;
+using AIWA.API.Integrations.GPT4;
+using AIWA.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace AIWA.API.Controllers;
 
@@ -13,6 +13,9 @@ public class GenerateController(ILogger<GenerateController> logger, UkesMailComp
 {
     private readonly ILogger<GenerateController> _logger = logger;
     private readonly UkesMailCompletion _ukesMailCompletion = ukesMailCompletion;
+    private const string TextEventStream = "text/event-stream";
+    private static readonly List<MediaTypeHeaderValue> _acceptStreamHeaders = [new(TextEventStream)];
+    private static readonly List<MediaTypeHeaderValue> _acceptJsonHeaders = [new(MediaTypeNames.Application.Json), new(""), new("*/*")];
     private const int MAX_INPUT_LINES = 5;
     private const int MAX_INPUT_CHARS = 1000;
 
@@ -22,40 +25,47 @@ public class GenerateController(ILogger<GenerateController> logger, UkesMailComp
     /// <param name="input">Custom input parameters.</param>
     /// <param name="accept">Response type.</param>
     /// <remarks>
-    /// Use the Accept header to specify the response type.
-    /// - "application/json" (default): receive the content as a HTTP response.
-    /// - "text/event-stream": receive the content as a chunked stream.
+    /// The Accept header is optional. By default, the method will return application/json.
+    /// - "application/json" (default): receive the content as JSON in a regular HTTP response.
+    /// - "text/event-stream": receive the content as plain text in a chunked stream.
     /// </remarks>
     [HttpPost("ukesmail")]
-    [Produces(MediaTypeNames.Application.Json, "text/event-stream")]
+    [Produces(MediaTypeNames.Application.Json, TextEventStream)]
     [ProducesResponseType(StatusCodes.Status413RequestEntityTooLarge)]
-    public async Task<IActionResult> PostAsync([FromHeader(Name = "accept")] string? accept, AIWAInput input, CancellationToken cancellationToken)
+    public async Task<IActionResult> PostAsync(AIWAInput input, CancellationToken cancellationToken)
     {
+
         var validationResult = ValidateInput(input);
         if (!validationResult.IsSuccess)
         {
-            return new ObjectResult(validationResult.Error) { StatusCode = (int)validationResult.Value };
+            return Problem
+            (
+               title: "Invalid input",
+               detail: validationResult.Error,
+               statusCode: (int)validationResult.Value
+            );
         }
 
-        switch (accept)
-        {
-            case MediaTypeNames.Application.Json:
-            case "":
-            case "*/*":
-            case null:
-                var messages = await _ukesMailCompletion.GenerateUkesMailAsync(input.Entries, choices: 1, cancellationToken);
-                return new JsonResult(new AIWAResponse { Content = messages.First().Content });
-            case "text/event-stream":
-                var streamingResult = await _ukesMailCompletion.GetUkesMailStreamingAsync(input.Entries, cancellationToken);
-                return new ChunkedStreamingChatChoiceResult(streamingResult);
-            default:
-                var problemDetails = new ProblemDetails
-                {
-                    Detail = $"'Accept' header specifies an Invalid format. Expected formats are '${MediaTypeNames.Application.Json}' (default) or 'text/event-stream'.",
-                    Status = 400
-                };
+        var requestAcceptHeader = HttpContext.Request.GetTypedHeaders().Accept;
 
-                return BadRequest(problemDetails);
+        if (requestAcceptHeader.Count == 0 || requestAcceptHeader.Any(_acceptStreamHeaders.Contains))
+        {
+            var streamingResult = await _ukesMailCompletion.GetUkesMailStreamingAsync(input.Entries, cancellationToken);
+            return new ChunkedStreamingChatChoiceResult(streamingResult);
+        }
+        else if (requestAcceptHeader.Any(_acceptJsonHeaders.Contains))
+        {
+            var messages = await _ukesMailCompletion.GenerateUkesMailAsync(input.Entries, choices: 1, cancellationToken);
+            return new JsonResult(new AIWAResponse { Content = messages.First().Content });
+        }
+        else
+        {
+            return Problem
+            (
+                title: "Invalid format",
+                detail: $"The format specified by the 'Accept' header is not valid. Expected formats are '${MediaTypeNames.Application.Json}' (default) or '${TextEventStream}'.",
+                statusCode: StatusCodes.Status400BadRequest
+            );
         }
     }
 
